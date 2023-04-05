a9e31a8b159d964d07ebdba8be023aed90ca2014
karan.h@softdebut.com <karan.h@softdebut.com>
2023-04-05 15:22:14 +0700

    add gitignore add devZone add kentico Form

---
+++ DancingGoatCore/Controllers/DevZoneController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DancingGoat.Controllers
+{
+    public class DevZoneController : Controller
+    {
+        [HttpGet("/devzone")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("/devzone/form")]
+        public IActionResult Form() { 
+            return View();  
+        }
+
+    }
+}
