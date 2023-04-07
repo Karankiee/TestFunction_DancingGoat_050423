88f36cd3d195340ed1f14d41bb153b02c1224b1c
karan.h@softdebut.com <karan.h@softdebut.com>
2023-04-07 13:14:50 +0700

    Create CustomFormComponent

---
+++ DancingGoatCore/Components/FormComponents/CustomFormComponent/CustomFormComponentProperties.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+using CMS.DataEngine;
+
+using Kentico.Forms.Web.Mvc;
+
+
+namespace DancingGoat.FormComponents
+{
+    public class CustomFormComponentProperties : FormComponentProperties<string>
+    {
+        // Initializes a new instance of the CustomFormComponentProperties class and configures the underlying database field
+        public CustomFormComponentProperties() : base(FieldDataType.Text, size: 200)
+        {
+        }
+
+        [DefaultValueEditingComponent(TextInputComponent.IDENTIFIER)]
+        public override string DefaultValue
+        {
+            get;
+            set;
+        }
+    }
+}
